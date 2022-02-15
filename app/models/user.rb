02b3925e49c9cd1f8a906c71656c14dd01f5c768
tyler <tymaggio03@gmail.com>
2022-02-15 13:46:03 -0700

    basic backend fw

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
     has_many :user_chores
     has_many :chores, through: :user_chores
 
+    validates :name, presence: true
+    validates :password, presence: true
     has_secure_password
 end
